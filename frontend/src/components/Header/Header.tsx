20f1e5da0b8a42461d2a194c92a7485a3ea3ec11
matyiii <mezofimate@gmail.com>
2024-03-29 18:41:47 +0100

    feat(frontend): add navigation header WIP

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,74 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useState } from 'react';
+import { useAppSelector } from '@/store/hooks';
+import { selectUser } from '@/store/auth/authSelector';
+
+const Header = () => {
+	const user = useAppSelector(selectUser);
+
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<>
+			<AppBar position='static'>
+				<Toolbar>
+					<IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+						Photos
+					</Typography>
+					{user.id && (
+						<div>
+							<IconButton
+								size='large'
+								aria-label='account of current user'
+								aria-controls='menu-appbar'
+								aria-haspopup='true'
+								onClick={handleMenu}
+								color='inherit'
+							>
+								<AccountCircle />
+							</IconButton>
+							<Menu
+								id='menu-appbar'
+								anchorEl={anchorEl}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'right'
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'right'
+								}}
+								onClose={handleClose}
+								open={!!anchorEl}
+							>
+								<MenuItem onClick={handleClose}>Profile</MenuItem>
+								<MenuItem onClick={handleClose}>My account</MenuItem>
+							</Menu>
+						</div>
+					)}
+				</Toolbar>
+			</AppBar>
+		</>
+	);
+};
+
+export default Header;
