9be2899095fb7b431899953eab90deb27eb5afb8
matyiii <mezofimate@gmail.com>
2024-07-07 12:24:06 +0200

    feat(frontend): add types for Images, Files, ThreeDModels

--- frontend/src/shared.d.ts
+++ frontend/src/shared.d.ts
@@ -32,3 +32,39 @@ type CategoryType = {
 	updated_at?: string;
 	created_at?: string;
 };
+
+type ImageType = {
+	id: number;
+	name: string;
+	path: string;
+	extension: string;
+	three_d_model_id: number;
+	created_at: string;
+	updated_at: string;
+};
+
+type FileType = {
+	id: number;
+	name: string;
+	path: string;
+	extension: string;
+	three_d_model_id: number;
+	created_at: string;
+	updated_at: string;
+};
+
+type ThreeDModelType = {
+	id: number;
+	name: string;
+	is_banned: boolean;
+	is_highlighted: boolean;
+	like_count: number;
+	user_id: number;
+	category_id: number;
+	created_at: string;
+	updated_at: string;
+	user: User;
+	category: CategoryType;
+	images: ImageType[];
+	files: FileType[];
+};
