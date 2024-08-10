614fdb038b37fa4458c7fb8e16ea2a7570a75ca5
matyiii <mezofimate@gmail.com>
2024-08-10 21:09:28 +0200

    feat(backend): add User seeder

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		User::factory()->count(5)->create();
+
+		User::create([
+			'name' => 'Admin User',
+			'email' => 'admin@admin.com',
+			'password' => bcrypt('password123'),
+		]);
+	}
+}
