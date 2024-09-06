57543852ed163a088724cf489236183a2c2f104c
mem8htv <fixed-term.MateArnold.Mezofi@hu.bosch.com>
2024-09-06 16:32:14 +0200

    feat(frontend): display most like Models on Welcome page

--- frontend/src/pages/Welcome/Welcome.tsx
+++ frontend/src/pages/Welcome/Welcome.tsx
@@ -1,24 +1,37 @@
 import DataService from '@/service/DataService';
-import { Button } from 'rsuite';
 import useUser from '@/hooks/useUser';
+import { useEffect, useState } from 'react';
+import Preview from '@/components/ThreeDModel/Preview/Preview';
 
 const Welcome = () => {
 	const { user } = useUser();
+	const [models, setModels] = useState<ThreeDModelType[]>();
+
+	useEffect(() => {
+		DataService.threeD
+			.getMostLikedModels()
+			.then((res) => {
+				console.log(res);
+				setModels(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
 	return (
-		<div>
-			{`Welcome - ${user.name}`}
-			<div className='content-div'>
-				<Button
-					onClick={() => {
-						DataService.dummy
-							.test()
-							.then((res) => console.log(res))
-							.catch((err) => console.log(err));
-					}}
-				>
-					TEST
-				</Button>
+		<div className='flex flex-col w-full h-full'>
+			<div className='flex justify-center bg-orange-300'>
+				<h1 className='text-6xl text-slate-400 font-bold my-6'>PrintIT</h1>
+			</div>
+			<div className='flex-1 bg-yellow-300'>
+				<div className='p-4'>
+					<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+						{models?.map((model) => {
+							return <Preview key={model.id} model={model} />;
+						})}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
