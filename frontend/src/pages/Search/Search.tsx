0e29951cf33d8232e7d1a1cacaf7edd7e4fe28e8
matyiii <mezofimate@gmail.com>
2024-09-22 15:48:52 +0200

    fix(frontend): handle empty search result, show query

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -6,9 +6,11 @@ import { useLocation } from 'react-router-dom';
 const Search = () => {
 	/* Hooks */
 	const { search } = useLocation();
+	const queryParams = new URLSearchParams(search);
+	const searchQuery = queryParams.get('q');
 
 	/* State */
-	const [models, setModels] = useState<ThreeDModelType[]>();
+	const [models, setModels] = useState<ThreeDModelType[]>([]);
 
 	/* Effects */
 	useEffect(() => {
@@ -25,15 +27,17 @@ const Search = () => {
 	}, [search]);
 
 	return (
-		<div className='flex flex-col w-full h-full bg-yellow-300'>
-			<div className='p-4'>
-				<h1 className='text-2xl font-bold text-center mb-4'>Discover Prints</h1>
+		<div className='p-4'>
+			<h1 className='text-2xl font-bold text-center mb-4'>{`Search Results for "${searchQuery}"`}</h1>
+			{models.length === 0 ? (
+				<p className='text-center text-gray-500'>No results found. Please try a different search.</p>
+			) : (
 				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-					{models?.map((model) => {
-						return <Preview key={model.id} model={model} />;
-					})}
+					{models.map((model) => (
+						<Preview key={model.id} model={model} />
+					))}
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
